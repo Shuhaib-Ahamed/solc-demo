c57755abc62c12a5f7b8559c0dc79e3ca9eb4644
Shuhaib-Ahamed <shuhaibsamadh@gmail.com>
2022-10-13 21:01:10 +0530

    added change

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 var jsonParser = bodyParser.json();
 
 const corsOptions = {
-  origin: "https://sixer-testing-beta.netlify.app/",
+  origin: "http://localhost:4200",
 };
 
 app.use(cors(corsOptions));
